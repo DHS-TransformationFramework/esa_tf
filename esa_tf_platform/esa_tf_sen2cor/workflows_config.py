4f183cb10aef297c8512a4339e5a970eadf19112
Aureliana Barghini <a.barghini@bopen.eu>
2021-10-07 15:05:56 +0200

    update definition in workflows_config.py

--- esa_tf_platform/esa_tf_sen2cor/workflows_config.py
+++ esa_tf_platform/esa_tf_sen2cor/workflows_config.py
@@ -1,18 +1,16 @@
-sen2cor_l1c_l2a_config = {
-    
+sen2cor_l1c_l2a = {
     "Id": "",
-    "Name": "Sen2Cor_L1C_L2A",
     "Description": "Product processing from Sentinel-2 L1C to L2A. Processor V2.3.6",
     "InputProductType": "S2MSILC",
     "OutputProductType": "S2MSI2A",
     "WorkflowVersion": "0.1",
     "WorkflowOptions": [
         {
-            "Name": "Aerosol_Type",
+            "Name": "aerosol_type",
             "Description": "Default processing via configuration is the rural (continental) aerosol type with mid latitude summer and an ozone concentration of 331 Dobson Units",
-            "Type": "str",
-            "Default": "RURAL",
-            "Value": ["MARITIME", "RURAL", "AUTO"],
+            "Type": "string",
+            "Default": "rural",
+            "Values": ["maritime", "rural", "auto"],
         },
     ],
 }
