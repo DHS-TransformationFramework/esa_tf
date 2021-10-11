e0095691f3ebe78bfd01e9f5fe5d784e8f593954
Aureliana Barghini <a.barghini@bopen.eu>
2021-10-11 11:25:23 +0200

    removed Id in config

--- esa_tf_platform/esa_tf_platform/esa_tf_plugin_sen2cor.py
+++ esa_tf_platform/esa_tf_platform/esa_tf_plugin_sen2cor.py
@@ -1,5 +1,4 @@
 sen2cor_l1c_l2a = {
-    "Id": "",
     "Description": "Product processing from Sentinel-2 L1C to L2A. Processor V2.3.6",
     "InputProductType": "S2MSILC",
     "OutputProductType": "S2MSI2A",
