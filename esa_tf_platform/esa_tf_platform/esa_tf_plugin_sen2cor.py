7a4be74fed76f21f154056ef1e890948228460b5
Marco Di Bari <m.dibari@bopen.eu>
2021-10-14 18:50:24 +0200

    WIP: Add checks about input products and user options.

--- esa_tf_platform/esa_tf_platform/esa_tf_plugin_sen2cor.py
+++ esa_tf_platform/esa_tf_platform/esa_tf_plugin_sen2cor.py
@@ -1,11 +1,16 @@
+import glob
+import logging
 import os
 from xml.etree import ElementTree
+import subprocess
 
 
 SEN2COR_CONFILE_NAME = "L2A_GIPP.xml"
 SRTM_DOWNLOAD_ADDRESS = (
     "http://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/"
 )
+MTD_FILENAME = "MTD_MSIL1C.xml"
+ROI_OPTIONS_NAMES = {"row0", "col0", "nrow_win", "ncol_win"}
 
 sen2cor_l1c_l2a = {
     "Description": "Product processing from Sentinel-2 L1C to L2A. Processor V2.3.6",
@@ -59,15 +64,15 @@ sen2cor_l1c_l2a = {
 }
 
 
-def set_sen2cor_options(etree, options, srtm_folder):
+def set_sen2cor_options(etree, options, srtm_path):
     """Replace in the input ElementTree object (representing the parsed default L2A_GIPP.xml
     configuration file) the values of tags that a user can specify as processing options, according
-    to the user desiderata specified by means of ``options``. The ``srtm_folder`` is the system
+    to the user desiderata specified by means of ``options``. The ``srtm_path`` is the system
     folder in which the SRTM DEM will be downloaded or, if DEM is already available, searched.
 
     :param ElementTree.ElementTree etree: the parsed default L2A_GIPP.xml configuration file
     :param dict options: dictionary of the user options
-    :param str srtm_folder: path of the folder in which the SRTM DEM will be downloaded
+    :param str srtm_path: path of the folder in which the SRTM DEM will be downloaded
     :return ElementTree.ElementTree:
     """
     for k, v in options.items():
@@ -83,26 +88,206 @@ def set_sen2cor_options(etree, options, srtm_folder):
             tag_name = "_".join([s.capitalize() for s in k.lower().split("_")])
             etree.findall(f".//{tag_name}")[0].text = str(v).upper()
         elif k.lower() == "dem_terrain_correction" and v:
-            etree.findall(".//DEM_Directory")[0].text = srtm_folder
+            etree.findall(".//DEM_Directory")[0].text = srtm_path
             etree.findall(".//DEM_Reference")[0].text = SRTM_DOWNLOAD_ADDRESS
     return etree
 
 
-def create_sen2cor_confile(processing_dir, srtm_folder, options):
+def create_sen2cor_confile(processing_dir, srtm_path, options):
     """Parse the default Sen2Cor L2A_GIPP.xml configuration file, set the user options and write
     the new configuration file relative to the current customisation in the processing-dir. The
     function returns the full path of the new created L2A_GIPP.xml file.
 
     :param str processing_dir: path of the processing directory
-    :param str srtm_folder: path of the folder in which the SRTM DEM will be downloaded
+    :param str srtm_path: path of the folder in which the SRTM DEM will be downloaded
     :param dict options: dictionary of the user options
     :return str:
     """
     # Read the default Sen2Cor configuration file
     sample_config_path = os.path.join(__package__, "resources", SEN2COR_CONFILE_NAME)
     et = ElementTree.parse(sample_config_path)
-    et = set_sen2cor_options(et, options, srtm_folder)
+    et = set_sen2cor_options(et, options, srtm_path)
     # Write back to file
     output_confile = os.path.join(processing_dir, SEN2COR_CONFILE_NAME)
     et.write(output_confile)
     return output_confile
+
+
+def check_input_consistency(product_folder_path):
+    """Check if the input product is a valid Sentinel-2 L1C product.
+
+    :param str product_folder_path:
+    :return:
+    """
+    if not os.path.isdir(product_folder_path):
+        raise ValueError(
+            f"{product_folder_path} is no a valid directory: specify the directory of a Sentinel-2 L1C input product"
+        )
+    mtd_file = glob.glob(MTD_FILENAME)
+    if len(mtd_file) != 1:
+        raise ValueError(
+            f"{product_folder_path} is an invalid input product. Please specify the directory of a Sentinel-2 L1C input product"
+        )
+    img_data = glob.glob(os.path.join(product_folder_path, "*", "*", "*", "*.jp2"))
+    if len(img_data) != 14:
+        raise ValueError(
+            f"the input product {product_folder_path} is not a valid Sentinel-2 L1C product"
+        )
+
+
+def find_option_definition(option_name):
+    """
+
+    :param str option_name:
+    :return dict:
+    """
+    for option_def in sen2cor_l1c_l2a["WorkflowOptions"]:
+        if option_def["Name"] == option_name:
+            return option_def
+    return None
+
+
+def check_ozone_content(options, valid_ozone_values):
+    """
+
+    :param dict options:
+    :param list_or_tuple valid_ozone_values:
+    :return:
+    """
+    ozone_winter_values = (0, 250, 290, 330, 377, 420, 460)
+    ozone_summer_values = (0, 250, 290, 331, 370, 410, 450)
+    ozone_content_value = options["ozone_content"]
+    mid_latitude_value = options["mid_latitude"]
+    if mid_latitude_value == "auto" and (ozone_content_value not in valid_ozone_values):
+        raise ValueError()
+    elif mid_latitude_value == "winter" and (
+        ozone_content_value not in ozone_winter_values
+    ):
+        raise ValueError()
+    elif mid_latitude_value == "summer" and (
+        ozone_content_value not in ozone_summer_values
+    ):
+        raise ValueError()
+    return True
+
+
+def check_row_col_type(roi_options):
+    """
+
+    :param dict roi_options:
+    :return bool:
+    """
+    # if row0 is a string, col0 must be a string with the same value
+    if isinstance(roi_options["row0"], str):
+        if not isinstance(roi_options["col0"], str):
+            raise ValueError("row0 and col0 must be or two string or two integers")
+        if roi_options["row0"] != roi_options["col0"]:
+            raise ValueError("if row0 and col0 values are string, they must be equal")
+    # if row0 is an integer, then, also col0 must be an integer
+    if isinstance(roi_options["row0"], int):
+        if not isinstance(roi_options["col0"], int):
+            raise ValueError("row0 and col0 must be or two string or two integers")
+        for oname in ["row0", "col0"]:
+            if roi_options[oname] % 6 != 0:
+                raise ValueError("row0, col0 must be integer divisible by 6")
+    return True
+
+
+def check_nrow_ncol(roi_options):
+    """
+
+    :param dict roi_options:
+    :return bool:
+    """
+    for oname in ["nrow_win", "ncol_win"]:
+        if roi_options[oname] % 6 != 0:
+            raise ValueError("nrow_win and ncol_win must be integer divisible by 6")
+    return True
+
+
+def check_roi_options(roi_options):
+    """
+
+    :param dict roi_options:
+    :return bool:
+    """
+    if not roi_options:
+        return True
+    # if ROI options are present, they must be 4, not just a few
+    if len(roi_options) != len(ROI_OPTIONS_NAMES):
+        missing_options = ROI_OPTIONS_NAMES.difference(set(roi_options.keys()))
+        raise ValueError(f"some ROI options are missing: {missing_options}")
+    check_row_col_type(roi_options)
+    check_nrow_ncol(roi_options)
+    return True
+
+
+def check_options(options):
+    """
+
+    :param dict options:
+    :return bool:
+    """
+    # ATM ROI options are not present in the workflow definition
+    roi_options = {k: v for k, v in options.items() if k in ROI_OPTIONS_NAMES}
+    other_options = {k: v for k, v in options.items() if k not in ROI_OPTIONS_NAMES}
+    check_roi_options(roi_options)
+    valid_names = [option["Name"] for option in sen2cor_l1c_l2a["WorkflowOptions"]]
+    for oname, ovalue in other_options.items():
+        if oname == "ozone_content":
+            check_ozone_content(
+                options, find_option_definition("ozone_content")["Values"]
+            )
+        elif oname in valid_names:
+            valid_values = find_option_definition(oname)["Values"]
+            if ovalue not in valid_values:
+                raise ValueError(
+                    f"invalid value '{ovalue}'' for '{oname}': valid values are {valid_values}"
+                )
+        else:
+            raise ValueError(f"invalid option {oname}: valid options are {valid_names}")
+    return True
+
+
+def run_processing(
+    product_folder_path,
+    processing_dir,
+    output_dir,
+    options,
+    sen2cor_path,
+    srtm_path,
+    logger=None,
+):
+    """
+
+    :param product_folder_path:
+    :param processing_dir:
+    :param output_dir:
+    :param options:
+    :param sen2cor_path:
+    :param srtm_path:
+    :param logger:
+    :return:
+    """
+    check_input_consistency(product_folder_path)
+    check_options(options)
+    sen2cor_confile = create_sen2cor_confile(processing_dir, srtm_path, options)
+    cmd = f"{sen2cor_path} {product_folder_path} --output_dir {output_dir} --GIP_L2A {sen2cor_confile}"
+    if "resolution" in options:
+        cmd += f" --resolution {options['resolution']}"
+    # > /dev/null 2>&1
+    stdout = open(os.devnull, "a")
+    stderr = open(os.devnull, "a")
+    if logger:
+        logger.info("Command line and its output ...\n\n" + cmd + "\n")
+        for hdlr in logger.handlers:
+            if isinstance(hdlr, logging.FileHandler):
+                # >> logfile_path 2>&1
+                outfile = open(hdlr.baseFilename, "a")
+                stdout = outfile
+                stderr = outfile
+    exit_status = subprocess.call(cmd, shell=True, stdout=stdout, stderr=stderr)
+    if exit_status != 0:
+        raise RuntimeError("Sen2Cor processing failed")
+    output_path = os.path.join(output_dir, os.listdir(output_dir))
+    return output_path
---
+++ esa_tf_platform/tests/test_esa_tf_plugin_sen2cor.py
@@ -0,0 +1,300 @@
+import os
+from xml.etree import ElementTree
+
+import pytest
+
+from esa_tf_platform import esa_tf_plugin_sen2cor
+
+
+VALID_OZONE_VALUES = [0, 250, 290, 330, 331, 370, 377, 410, 420, 450, 460]
+
+
+def test_set_sen2cor_options():
+    sample_config_path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "esa_tf_platform",
+        "resources",
+        "L2A_GIPP.xml",
+    )
+    et = ElementTree.parse(sample_config_path)
+
+    # check values before setting the user options
+    assert et.findall(".//Aerosol_Type")[0].text == "RURAL"
+    assert et.findall(".//Mid_Latitude")[0].text == "SUMMER"
+    assert et.findall(".//Ozone_Content")[0].text == "331"
+    assert et.findall(".//Cirrus_Correction")[0].text == "FALSE"
+
+    options = {
+        "aerosol_type": "maritime",
+        "mid_latitude": "auto",
+        "ozone_content": 0,
+        "cirrus_correction": True,
+    }
+    srtm_path = ""
+    et_with_options = esa_tf_plugin_sen2cor.set_sen2cor_options(et, options, srtm_path)
+
+    # check values after setting the user options
+    assert et_with_options.findall(".//Aerosol_Type")[0].text == "MARITIME"
+    assert et_with_options.findall(".//Mid_Latitude")[0].text == "AUTO"
+    assert et_with_options.findall(".//Ozone_Content")[0].text == "0"
+    assert et_with_options.findall(".//Cirrus_Correction")[0].text == "TRUE"
+
+
+def test_set_sen2cor_options_dem():
+    sample_config_path = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        "esa_tf_platform",
+        "resources",
+        "L2A_GIPP.xml",
+    )
+    et = ElementTree.parse(sample_config_path)
+
+    # check values before setting the user options
+    assert et.findall(".//DEM_Directory")[0].text == "NONE"
+    assert et.findall(".//DEM_Reference")[0].text == "NONE"
+
+    options = {"dem_terrain_correction": True}
+    srtm_path = "/dummy_folder/very_dummy_folder"
+    et_with_options = esa_tf_plugin_sen2cor.set_sen2cor_options(et, options, srtm_path)
+
+    # check values after setting the user options
+    assert et_with_options.findall(".//DEM_Directory")[0].text == srtm_path
+    assert (
+        et_with_options.findall(".//DEM_Reference")[0].text
+        == esa_tf_plugin_sen2cor.SRTM_DOWNLOAD_ADDRESS
+    )
+
+
+def test_create_sen2cor_confile(tmpdir):
+    processing_dir = tmpdir.join("processing_dir").strpath
+    os.mkdir(processing_dir)
+    srtm_path = tmpdir.join("dem").strpath
+    os.mkdir(srtm_path)
+    options = {
+        "aerosol_type": "maritime",
+        "mid_latitude": "auto",
+        "ozone_content": 0,
+        "cirrus_correction": True,
+    }
+    output_confile = esa_tf_plugin_sen2cor.create_sen2cor_confile(
+        processing_dir, srtm_path, options
+    )
+
+    assert os.path.isfile(output_confile)
+
+    et = ElementTree.parse(output_confile)
+    # check values after setting the user options
+    assert et.findall(".//Aerosol_Type")[0].text == "MARITIME"
+    assert et.findall(".//Mid_Latitude")[0].text == "AUTO"
+    assert et.findall(".//Ozone_Content")[0].text == "0"
+    assert et.findall(".//Cirrus_Correction")[0].text == "TRUE"
+
+
+def test_find_option_definition():
+    options_names = [
+        "aerosol_type",
+        "mid_latitude",
+        "ozone_content",
+        "cirrus_correction",
+        "dem_terrain_correction",
+        "resolution",
+    ]
+    for oname in options_names:
+        odef = esa_tf_plugin_sen2cor.find_option_definition(oname)
+        assert type(odef) is dict
+        assert odef["Name"].lower() == oname
+
+
+def test_check_ozone_content_valid_summer():
+    options = {
+        "mid_latitude": "summer",
+        "ozone_content": 370,
+        "cirrus_correction": True,
+    }
+    assert (
+        esa_tf_plugin_sen2cor.check_ozone_content(options, VALID_OZONE_VALUES) is True
+    )
+
+
+def test_check_ozone_content_invalid_summer():
+    options = {
+        "mid_latitude": "summer",
+        "ozone_content": 377,
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        esa_tf_plugin_sen2cor.check_ozone_content(options, VALID_OZONE_VALUES)
+
+
+def test_check_ozone_content_valid_winter():
+    options = {
+        "mid_latitude": "winter",
+        "ozone_content": 420,
+        "cirrus_correction": True,
+    }
+    assert (
+        esa_tf_plugin_sen2cor.check_ozone_content(options, VALID_OZONE_VALUES) is True
+    )
+
+
+def test_check_ozone_content_invalid_winter():
+    options = {
+        "mid_latitude": "winter",
+        "ozone_content": 410,
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        esa_tf_plugin_sen2cor.check_ozone_content(options, VALID_OZONE_VALUES)
+
+
+def test_check_ozone_content_valid_auto():
+    options = {
+        "mid_latitude": "auto",
+        "ozone_content": 290,
+        "cirrus_correction": True,
+    }
+    assert (
+        esa_tf_plugin_sen2cor.check_ozone_content(options, VALID_OZONE_VALUES) is True
+    )
+
+
+def test_check_ozone_content_invalid_auto():
+    options = {
+        "mid_latitude": "auto",
+        "ozone_content": 1000,
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        esa_tf_plugin_sen2cor.check_ozone_content(options, VALID_OZONE_VALUES)
+
+
+def test_check_roi_options_missing_valid():
+    roi_options = {}
+    assert esa_tf_plugin_sen2cor.check_roi_options(roi_options) is True
+
+
+def test_check_roi_options_missing_invalid():
+    roi_options = {
+        "row0": "off",
+        "col0": "off",
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        esa_tf_plugin_sen2cor.check_roi_options(roi_options)
+
+
+def test_check_roi_options_rowcol_different_type():
+    roi_options = {
+        "row0": "off",
+        "col0": 60,
+        "nrow_win": 600,
+        "ncol_win": 600,
+    }
+    with pytest.raises(ValueError):
+        esa_tf_plugin_sen2cor.check_roi_options(roi_options)
+
+
+def test_check_roi_options_rowcol_different_str_value():
+    roi_options = {
+        "row0": "off",
+        "col0": "auto",
+        "nrow_win": 600,
+        "ncol_win": 600,
+    }
+    with pytest.raises(ValueError):
+        esa_tf_plugin_sen2cor.check_roi_options(roi_options)
+
+
+def test_check_roi_options_rowcol_same_str_value():
+    roi_options = {
+        "row0": "off",
+        "col0": "off",
+        "nrow_win": 600,
+        "ncol_win": 600,
+    }
+    assert esa_tf_plugin_sen2cor.check_roi_options(roi_options) is True
+
+
+def test_check_roi_options_rowcol_int_value():
+    roi_options = {
+        "row0": 600,
+        "col0": 1200,
+        "nrow_win": 600,
+        "ncol_win": 600,
+    }
+    assert esa_tf_plugin_sen2cor.check_roi_options(roi_options) is True
+
+
+def test_check_roi_options_int_invalid_centre():
+    options = {
+        "row0": 311,  # wrong value, it is not divisible by 6
+        "col0": 1200,
+        "nrow_win": 650,
+        "ncol_win": 600,
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        assert esa_tf_plugin_sen2cor.check_roi_options(options)
+
+
+def test_check_roi_options_int_invalid_win():
+    options = {
+        "row0": 600,
+        "col0": 1200,
+        "nrow_win": 650,  # wrong value, it is not divisible by 6
+        "ncol_win": 600,
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        assert esa_tf_plugin_sen2cor.check_roi_options(options)
+
+
+def test_check_options_valid_no_roi():
+    options = {
+        "aerosol_type": "maritime",
+        "mid_latitude": "auto",
+        "ozone_content": 0,
+        "cirrus_correction": True,
+    }
+    assert esa_tf_plugin_sen2cor.check_options(options) is True
+
+
+def test_check_options_invalid_no_roi():
+    options = {
+        "aerosol_type": "dummy",
+        "mid_latitude": "auto",
+        "ozone_content": 0,
+        "cirrus_correction": True,
+    }
+    with pytest.raises(ValueError):
+        assert esa_tf_plugin_sen2cor.check_options(options)
+
+
+def test_check_options_invalid_with_roi():
+    options = {
+        "aerosol_type": "maritime",
+        "mid_latitude": "dummy",
+        "ozone_content": 0,
+        "cirrus_correction": True,
+        "row0": 600,
+        "col0": 1200,
+        "nrow_win": 600,
+        "ncol_win": 600,
+    }
+    with pytest.raises(ValueError):
+        assert esa_tf_plugin_sen2cor.check_options(options)
+
+
+def test_check_options_invalid_with_roi2():
+    options = {
+        "aerosol_type": "maritime",
+        "mid_latitude": "dummy",
+        "ozone_content": 0,
+        "cirrus_correction": True,
+        "row0": "off",
+        "col0": 1200,
+        "nrow_win": 600,
+        "ncol_win": 600,
+    }
+    with pytest.raises(ValueError):
+        assert esa_tf_plugin_sen2cor.check_options(options)
