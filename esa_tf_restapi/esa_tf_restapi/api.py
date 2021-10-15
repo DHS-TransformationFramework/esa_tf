2ab43109c52f292731537fdd455b60334cf55a23
Aureliana Barghini <a.barghini@bopen.eu>
2021-10-15 09:51:14 +0200

    retrieve dask_scheduler from the enviroment

--- esa_tf_restapi/esa_tf_restapi/api.py
+++ esa_tf_restapi/esa_tf_restapi/api.py
@@ -1,9 +1,15 @@
+import os
+
 import dask.distributed
 
 CLIENT = None
 
 
-def instantiate_client(dask_scheduler):
+def instantiate_client(dask_scheduler=None):
+    if dask_scheduler is None:
+        dask_scheduler = os.getenv("SCHEDULER")
+    if dask_scheduler is None:
+        raise ValueError("Scheduler not defined")
     global CLIENT
     if CLIENT is None:
         CLIENT = dask.distributed.Client(dask_scheduler)
@@ -33,3 +39,23 @@ def get_order_status(order_id, scheduler=None):
     status = client.cluster.scheduler.get_task_status(keys=[order_id])[order_id]
     return status
 
+
+def submit_workflow(
+    workflow_id,
+    *,
+    product,
+    workflow_kwargs,
+    scheduler=None
+):
+    """Submit a workflow  on a product and returns the order_id
+    """
+    def task():
+        import esa_tf_platform
+        return esa_tf_platform.run(
+            workflow_id,
+            workflow_kwargs=workflow_kwargs,
+            product=product,
+        )
+    client = instantiate_client(scheduler)
+    future = client.submit(task)
+    return future.key
