00adce895c260106289aa3e7c64e378d5768ac9b
Aureliana Barghini <a.barghini@bopen.eu>
2022-01-24 21:44:08 +0100

    fix order in WorkflowOption and add Enum for bool types

--- esa_tf_platform/esa_tf_platform/esa_tf_plugin_sen2cor.py
+++ esa_tf_platform/esa_tf_platform/esa_tf_plugin_sen2cor.py
@@ -302,7 +302,7 @@ sen2cor_l1c_l2a = {
             "Enum": ["MARITIME", "RURAL"],
         },
         "Mid_Latitude": {
-            "Description": "If  'AUTO' the atmosphere profile will be determined automatically by the processor, selecting WINTER or SUMMER atmosphere profile based on the acquisition date and geographic location of the tile",
+            "Description": "If 'AUTO' the atmosphere profile will be determined automatically by the processor, selecting WINTER or SUMMER atmosphere profile based on the acquisition date and geographic location of the tile",
             "Type": "string",
             "Default": "SUMMER",
             "Enum": ["SUMMER", "WINTER", "AUTO"],
@@ -315,19 +315,21 @@ sen2cor_l1c_l2a = {
         },
         "Cirrus_Correction": {
             "Description": "FALSE: no cirrus correction applied, TRUE: cirrus correction applied",
-            "Type": "boolean",
             "Default": False,
+            "Type": "boolean",
+            "Enum": [True, False]
         },
         "DEM_Terrain_Correction": {
             "Description": "Use DEM for Terrain Correction, otherwise only used for WVP and AOT",
-            "Type": "boolean",
             "Default": True,
+            "Type": "boolean",
+            "Enum": [True, False]
         },
         "Resolution": {
             "Description": "Target resolution, can be 10, 20 or 60m. If omitted, 10, 20 and 60m resolutions will be processed",
+            "Default": None,
             "Type": "integer",
             "Enum": [10, 20, 60],
-            "Default": None,
         },
     },
 }
