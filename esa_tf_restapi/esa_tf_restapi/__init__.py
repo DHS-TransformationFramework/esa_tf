c8bc93d6fb54ad252fa0d009475e0520b50f5139
Aureliana Barghini <a.barghini@bopen.eu>
2022-05-11 15:18:56 +0200

    prepare release v0.9.2-osf

--- esa_tf_restapi/esa_tf_restapi/__init__.py
+++ esa_tf_restapi/esa_tf_restapi/__init__.py
@@ -19,7 +19,7 @@ from fastapi import FastAPI, Header
 from fastapi.responses import JSONResponse
 from odata_query.exceptions import ODataException
 
-__version__ = "0.9.2"
+__version__ = "0.9.2-osf"
 
 app = FastAPI(root_path=os.environ.get("ROOT_PATH", ""))
 
